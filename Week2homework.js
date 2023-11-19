cf77c8b24cb76be0eb47d114211dd71eaf926d9f
Nathan Gordon <gordon.nathan333@gamil.com>
2023-11-19 16:17:08 -0600

    Completed the Bonus work for Week 2

--- Week2homework.js
+++ Week2homework.js
@@ -1,5 +1,5 @@
 let pizzaPlace = "Pizza Palace";
-let numberOfToppings = 3;
+let numberOfToppings = 6;
 
 console.log(pizzaPlace, typeof pizzaPlace);
 console.log(numberOfToppings, typeof numberOfToppings);
@@ -10,4 +10,10 @@ if (numberOfToppings < 10) {
   console.log("Quality, not quantity.");
 } else {
   console.log("A whole lot of pizza.");
+}
+
+for (let i = 1; i <= numberOfToppings; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
 }
