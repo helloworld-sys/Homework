392156c1cf97edf84a88b3f0fb836ba016b40d2e
Nathan Gordon <gordon.nathan333@gamil.com>
2023-11-19 15:49:28 -0600

    Week 2 homework initial commit

---
+++ Week2homework.js
@@ -0,0 +1,13 @@
+let pizzaPlace = "Pizza Palace";
+let numberOfToppings = 3;
+
+console.log(pizzaPlace, typeof pizzaPlace);
+console.log(numberOfToppings, typeof numberOfToppings);
+
+console.log(`The ${pizzaPlace} has ${numberOfToppings} toppings!`);
+
+if (numberOfToppings < 10) {
+  console.log("Quality, not quantity.");
+} else {
+  console.log("A whole lot of pizza.");
+}
